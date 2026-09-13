25ecc61bf2590e5a4ffe2df4f9de85cc75455340
khalifeh basiri <105331419+khalifehbasiri@users.noreply.github.com>
2026-09-12 21:26:20 -0400

    Add accepted solution for Valid Anagram

---
+++ 0242-valid-anagram/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        char_s = {}
+        char_t = {}
+
+        if len(s) != len(t):
+            return False
+
+        for char in s:
+            if char in char_s:
+                char_s[char] += 1
+            else:
+                char_s[char] = 1
+        
+        for char in t:
+            if char in char_t:
+                char_t[char] += 1
+            else:
+                char_t[char] = 1
+        
+        return char_s == char_t
+
