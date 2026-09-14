8523847bb7fbc401021a9a90ebf84cb0d9df5bd7
khalifeh basiri <105331419+khalifehbasiri@users.noreply.github.com>
2026-09-14 00:54:09 -0400

    Add accepted solution for Two Sum II - Input Array Is Sorted

--- 0167-two-sum-ii-input-array-is-sorted/solution.py
+++ 0167-two-sum-ii-input-array-is-sorted/solution.py
@@ -3,21 +3,13 @@ class Solution:
         i = 0
         j = len(numbers) - 1
 
-        num1 = 0
-        num2 = 0
         while i < j:
             if numbers[i] + numbers[j] == target:
-                num1 = i
-                num2 = j
-                break
+                return [i + 1, j + 1]
             elif numbers[i] + numbers[j] > target:
                 j -= 1
             else:
                 i += 1
-
-        num1 += 1
-        num2 += 1
-        return [num1, num2]
             
 
 
