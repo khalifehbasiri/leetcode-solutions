f664abbc097302b0524f4d32d1c290fe7f9c3470
khalifeh basiri <105331419+khalifehbasiri@users.noreply.github.com>
2026-09-14 01:58:28 -0400

    Add accepted solution for Maximum Average Subarray I

--- 0643-maximum-average-subarray-i/solution.py
+++ 0643-maximum-average-subarray-i/solution.py
@@ -8,7 +8,7 @@ class Solution:
 
         while j < len(nums):
             sums = sums - nums[i-1] + nums[j]
-            max_total = sums if sums > max_total else max_total
+            max_total = max(sums, max_total)
             i += 1
             j += 1
         return max_total / k
