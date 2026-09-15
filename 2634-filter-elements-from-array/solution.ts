aedadf8e22752b0bc61140d863981504831a41f6
khalifeh basiri <105331419+khalifehbasiri@users.noreply.github.com>
2026-09-14 20:33:43 -0400

    Add accepted solution for Filter Elements from Array

--- 2634-filter-elements-from-array/solution.ts
+++ 2634-filter-elements-from-array/solution.ts
@@ -1,5 +1,7 @@
 type Fn = (n: number, i: number) => any
 
 function filter(arr: number[], fn: Fn): number[] {
-    return arr.filter((n, i) => Boolean(fn(n,i)))
+    const results: number[] = [];
+    arr.forEach((x, i) => { Boolean(fn(x, i)) && results.push(x)})
+    return results
 };
