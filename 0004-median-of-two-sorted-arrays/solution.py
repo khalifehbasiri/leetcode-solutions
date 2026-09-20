42577088299803fde477d3486ec28d6b4a358440
khalifeh basiri <105331419+khalifehbasiri@users.noreply.github.com>
2026-09-20 15:37:06 -0400

    Add accepted solution for Median of Two Sorted Arrays

--- 0004-median-of-two-sorted-arrays/solution.py
+++ 0004-median-of-two-sorted-arrays/solution.py
@@ -1,9 +1,8 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        nums = sorted(nums1 + nums2)
+        middle = len(nums) // 2
         
-        SortNums = sorted(nums1+nums2)
+        return float(nums[middle]) if (len(nums) % 2 == 1) else (nums[middle - 1] + nums[middle]) / 2
 
-        if len(SortNums) % 2 != 0:
-            return SortNums[len(SortNums) // 2]
-        else:
-            return (SortNums[len(SortNums) // 2 - 1] + SortNums[len(SortNums) // 2]) / 2
+
