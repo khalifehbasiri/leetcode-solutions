9a8364736868e9cc4a60681b4dc88a419a626dbc
khalifeh basiri <105331419+khalifehbasiri@users.noreply.github.com>
2026-09-20 00:30:20 -0400

    Add accepted solution for Valid Sudoku

---
+++ 0036-valid-sudoku/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        columns = {i: [] for i in range(9)}
+        rows = {i: [] for i in range(9)}
+        boxes = {(r, c): [] for r in range(3) for c in range(3)}
+
+        i = 0
+        j = 0
+        while i < 9:
+            while j < 9:
+                if board[i][j] != ".":
+                    rows[i].append(board[i][j])
+                    columns[j].append(board[i][j])
+                    boxes[(i // 3, j // 3)].append(board[i][j]) 
+                j += 1
+
+            j = 0
+            i += 1
+
+        for elem in columns:
+            if len(set(columns[elem])) != len(columns[elem]):
+                return False
+
+        for elem in rows:
+            if len(set(rows[elem])) != len(rows[elem]):
+                return False
+        
+        for elem in boxes:
+            if len(set(boxes[elem])) != len(boxes[elem]):
+                return False
+
+        return True
